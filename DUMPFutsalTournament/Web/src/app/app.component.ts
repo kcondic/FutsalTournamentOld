6cc8e6ffdc0311e49c221e2e52b11e4aa265a8a5
Bruno Vego <bruno@dump.hr>
2018-08-31 14:08:37 +0200

    Added mobile navigation styles

--- DUMPFutsalTournament/Web/src/app/app.component.ts
+++ DUMPFutsalTournament/Web/src/app/app.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+	constructor(private renderer: Renderer2) {}
+
+	toggleNavigation() {
+		const navigation = document.getElementById('nav');
+		const toggleNav = document.getElementById('toggle-nav');
+
+		if (toggleNav.className.includes('toggled')) {
+			this.renderer.removeClass(toggleNav, 'toggled');
+			this.renderer.removeClass(navigation, 'expanded');
+		} else {
+			this.renderer.addClass(toggleNav, 'toggled');
+			this.renderer.addClass(navigation, 'expanded');
+		}
+	}
+}
